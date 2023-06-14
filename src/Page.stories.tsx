1b93dcca5a6c3dba4601501f2b6113cd3cf15df9
hanatokumoto <hanako.tokumoto@sumally.com>
2023-06-14 13:47:08 +0900

    page を追加

---
+++ src/Page.stories.tsx
@@ -0,0 +1,5 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+
+import { Page } from './Page'
+export default { component: Page } as ComponentMeta<typeof Page>
+export const Standard: ComponentStoryObj<typeof Page> = {}
